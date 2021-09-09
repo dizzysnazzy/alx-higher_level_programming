3f14ebb8136da5fbf0f3c2fec43510749af01b7f
dizzysnazzy <dismaskipchumba2@gmail.com>
2021-09-09 19:27:15 +0000

    import modules file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,11 +2,15 @@
 # 4-hidden_discovery.py
 # Dismas Kipchumba <dismaskipchumba2@gmail.com>
 
-if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
-    import hidden_4
+import hidden_4
 
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+
+def discovr():
+    name = dir(hidden_4)
+    for i in name:
+        if i[:2] != '__':
+            print("{:s}".format(i))
+
+
+if __name__ == "__main__":
+    discovr()
