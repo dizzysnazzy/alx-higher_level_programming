e7cc2f8c62cf6f59f98ff3f434228fad89f9402c
dizzysnazzy <32930626+dizzysnazzy@users.noreply.github.com>
2021-10-05 16:21:25 +0300

    Update 102-tests.py

--- 0x07-python-test_driven_development/tests/102-tests.py
+++ 0x07-python-test_driven_development/tests/102-tests.py
@@ -4,15 +4,15 @@ lib = ctypes.CDLL('./libPython.so')
 lib.print_python_string.argtypes = [ctypes.py_object]
 s = "The spoon does not exist"
 lib.print_python_string(s)
-s = ""
+s = "ложка не существует"
 lib.print_python_string(s)
-s = "La cuill (re n'existe pas"
+s = "La cuillère n'existe pas"
 lib.print_python_string(s)
-s = ""
+s = "勺子不存在"
 lib.print_python_string(s)
-s = ""
+s = "숟가락은 존재하지 않는다."
 lib.print_python_string(s)
-s = ""
+s = "スプーンは存在しない"
 lib.print_python_string(s)
 s = b"The spoon does not exist"
-lib.print_python_string(s))
+lib.print_python_string(s)
