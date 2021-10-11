f092a4392f34d77cc0bdc781aac9c2b90c18f5fb
dizzysnazzy <dismaskipchumba2@gmail.com>
2021-10-11 09:26:38 +0000

    inheritence file

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+# Dismas Kipchumba <dismaskipchumba2@gmail.com>
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
