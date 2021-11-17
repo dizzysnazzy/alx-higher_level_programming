51124221d4bd990e3ecf3d1da1607b27b7ca73f9
dizzysnazzy <dismaskipchumba2@gmail.com>
2021-11-17 11:41:45 -0800

    mod file

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,5 @@
--- Creates the user user_0d_1 with all privileges.
-CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-   ON database.*
-   TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
+-- creates the MySQL server user user_0d_1
+-- creates a new user
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+-- grants privileges
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
